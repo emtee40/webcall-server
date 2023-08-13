1bf0524cba5eab022e7e6ad563ecabe4250ad579
timur.mehrvarz <timur@timur.mobi>
2023-08-13 19:00:44 +0200

    clientVersion = '4.0.0-beta5'

--- webroot/callee/client.js
+++ webroot/callee/client.js
@@ -1,6 +1,6 @@
 // WebCall Copyright 2023 timur.mobi. All rights reserved.
 'use strict';
-const clientVersion = '4.0.0-beta4';
+const clientVersion = '4.0.0-beta5';
 
 const avSelect = document.querySelector("select#avSelect");
 const vresDialogElement = document.getElementById('vresDialog');
